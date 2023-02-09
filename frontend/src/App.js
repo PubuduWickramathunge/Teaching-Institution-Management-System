26b9d9c32f496d2e4be28ca552900b3a0633be31
PubuduWickramathunge <pubuduwickramathunge99@gmail.com>
2023-02-09 13:33:04 +0530

    using react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import UserRegister from './components/login/UserRegister';
+import { BrowserRouter as  Router, Routes, Route, Link} from "react-router-dom";
+import Login from "./components/login/login";
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <div className="App">
-      Hi
+      
+        <Router>
+          <Routes>
+           
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/login/Userregister" element={<UserRegister/>} />
+              <Route path="/login/" element={<Login/>} />
+            
+          </Routes>
+        </Router>
+      
     </div>
   );
 }
